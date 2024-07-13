0ead4893361316ed97128c332448dcfb91126613
Rodrigo Trujillo Mirano <rodrigo.trumir@gmail.com>
2024-07-13 07:45:59 -0500

    args y kwargs

--- dia1/variables.py
+++ dia1/variables.py
@@ -3,3 +3,11 @@ nombre = "Rodrigo"
 edad = 22
 edad = [121,21,212]
 print(len(edad))
+
+
+# args son parametros multiples que se printean como tupla, kwargs es un diccionario
+def combinada(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+combinada(1,2,3,4,nombre="rodrigo",apellido="trujillo")
