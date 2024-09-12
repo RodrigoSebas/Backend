3ab848dbd502f54452fd363e740ec4db5e309297
Rodrigo Trujillo Mirano <rodrigo.trumir@gmail.com>
2024-09-12 17:40:19 -0500

    Siguiendo

--- src/instancias.js
+++
@@ -1,6 +0,0 @@
-import prisma from "@prisma/client";
-import AWS from "aws-sdk";
-
-export const conexion = new prisma.PrismaClient();
-
-
--- src/serializers/equipo.serializer.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const crearEquipoSerializer = Joi.object({
-    nombre: Joi.string().required(),
-    imagenId: Joi.string().optional(),
-})
